064a463e3bf677e2892274d8a6f234134050f44b
amanimavu <amanimavu@gmail.com>
2025-11-04 19:29:44 +0300

    feat: add Laravel Sanctum for API authentication and create personal access tokens migration
    - create an api route for getting all users

---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return User::all()->toResourceCollection();
+});
+// ->middleware('auth:sanctum');
