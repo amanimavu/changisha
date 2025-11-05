e85f52e9822a8a4bd14b754f628fb0a97a0da0c1
amanimavu <amanimavu@gmail.com>
2025-11-05 17:25:48 +0300

    feat: make address field nullable in users table migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('username')->unique();
-            $table->string('address');
+            $table->string('address')->nullable();
             $table->string('email')->unique();
             $table->string('phone')->unique();
             $table->timestamp('email_verified_at')->nullable();
