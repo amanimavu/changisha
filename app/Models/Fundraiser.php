871272bb5223979bfc4196f8a2fbcd97366d50f5
amanimavu <amanimavu@gmail.com>
2025-11-07 14:04:08 +0300

    feat: create Fundraiser model with mass assignable attributes and user relationship

---
+++ app/Models/Fundraiser.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Fundraiser extends Model
+{
+    /** @use HasFactory<\Database\Factories\FundraiserFactory> */
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'user_id',
+        'profile_picture',
+        'dob',
+        'id_number',
+    ];
+
+    /**
+     * Get user that owns the fundraiser
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
