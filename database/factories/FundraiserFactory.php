9e29d368cc61aec12a0f1c0b3dddf748109d273a
amanimavu <amanimavu@gmail.com>
2025-11-14 18:55:28 +0300

    feat: change factory definition for profile picture in fundraiser

--- database/factories/FundraiserFactory.php
+++ database/factories/FundraiserFactory.php
@@ -19,7 +19,7 @@ class FundraiserFactory extends Factory
     {
         return [
             "user_id" => User::factory(),
-            "profile_picture" => fake()->image($dir = "/images"),
+            "profile_picture" => fake()->imageUrl(category: "user"),
             "dob" => fake()->date(),
             "id_number" => fake()->unique()->randomNumber(6, true)
         ];
