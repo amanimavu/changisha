b2bb95cc5604f385ee7d762a46c88ccd07ab958c
amanimavu <amanimavu@gmail.com>
2025-11-14 18:56:07 +0300

    feat: define Fundraiser has many campaigns relationship

--- app/Models/Fundraiser.php
+++ app/Models/Fundraiser.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Fundraiser extends Model
@@ -30,5 +31,10 @@ class Fundraiser extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function campaigns(): HasMany
+    {
+        return $this->hasMany(Campaign::class);
     }
 }
